f604d802ab02450ac8b452d27f9ff27145d72409
shivam <mandjoai123@gmail.com>
2024-06-11 20:02:47 +0530

    index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import feedbackRoutes from './routers/feedbackRoutes';
 
 const app = express();
 const port = 5000;
-
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 app.use(express.json());
 app.use('/feedback', feedbackRoutes);
 
