7730b209e7881ce3b275fe2b79d9207b12cb2608
shivam <mandjoai123@gmail.com>
2024-06-12 14:27:22 +0530

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 import feedbackRoutes from './routers/feedbackRoutes';
 
 const app = express();
 const port = 5000;
+
+// Enable CORS with specific options
+const corsOptions = {
+  origin: 'https://feedback-frontend-puce.vercel.app/', 
+  methods: 'GET,POST', 
+  allowedHeaders: 'Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
 app.use(express.json());
 app.use('/feedback', feedbackRoutes);
 
